bab865f850e22aab851725d3d4992bf0a8b92782
Fenix4088 <fenix4088@gmail.com>
2022-05-09 09:28:56 +0300

    added SR colors

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.scss';
+import s from './Test.module.scss';
 
 function App() {
     return (
         <div className="App">
+            <p className={s['hello']}>Hello Searates</p>
         </div>
     );
 }
