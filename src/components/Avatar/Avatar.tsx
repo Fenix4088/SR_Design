8ef99fd7d6c3ea08bee73deb99bb2b6b4402f189
Fenix4088 <fenix4088@gmail.com>
2022-05-10 17:36:25 +0300

    Avatar: add and remove handlers

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -9,6 +9,8 @@ type Color = 'new-year' | 'birthday' | 'extrovert' | 'introvert';
 
 
 interface AvatarBase extends Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'color'> {
+    onAdd?: (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void
+    onRemove?: (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void
 }
 
 interface IAvatar extends AvatarBase {
@@ -27,7 +29,7 @@ type Overload = {
     (props: Logo): JSX.Element;
 }
 
-export const Avatar: Overload = ({size, color, type, className, userName, ...props}: any) => {
+export const Avatar: Overload = ({size, color, type, className, userName, onAdd, onRemove, ...props}: any) => {
 
     const avatarCN = classNames(styles['avatar'], styles[size], styles[color], className);
     const logoCN = classNames(styles['avatar'], styles['logo'], styles['huge'], className);
@@ -38,14 +40,21 @@ export const Avatar: Overload = ({size, color, type, className, userName, ...pro
     const userNameNodeContent = isLogoType ? 'Add logo' : userNameFirstLetter;
     const isControlPanelVisible = isLogoType || size === 'huge';
 
+    const onAddHandler = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>): void => {
+        onAdd(e)
+    }
+    const onRemoveHandler = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>): void => {
+        onRemove(e)
+    }
+
     //TODO: Maby should create a component for the control panel?
     const controlPanel = isControlPanelVisible ? <div className={styles['control-panel']}>
         <div className={styles['control-btn-wrapper']}>
-            <span className={styles['add-btn']}>
-                <CameraIcon />
+            <span className={styles['add-btn']} onClick={onAddHandler}>
+                <CameraIcon/>
             </span>
-            <span className={styles['remove-btn']}>
-                <TrashIcon />
+            <span className={styles['remove-btn']} onClick={onRemoveHandler}>
+                <TrashIcon/>
             </span>
         </div>
 
